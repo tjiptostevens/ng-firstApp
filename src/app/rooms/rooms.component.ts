41e5a40e4b1c0bab45a128275fa8afdafcb6cd58
tjiptostevens <steveneonites@gmail.com>
2023-03-28 17:18:05 +0700

    add room

--- src/app/rooms/rooms.component.ts
+++ src/app/rooms/rooms.component.ts
@@ -1,5 +1,5 @@
 import { Component } from '@angular/core';
-import { Room } from './rooms';
+import { Room, RoomList } from './rooms';
 
 @Component({
   selector: 'app-rooms',
@@ -16,6 +16,37 @@ export class RoomsComponent {
     availableRooms:10,
     bookedRooms:5
   }
+
+  roomList : RoomList[]=[
+    {
+      roomNumber:101,
+      roomType:'Deluxe Room',
+      amenities:'Air Conditioner, Free Wi-Fi, TV, Bathroom, Kitchen',
+      price:500,
+      photos:'https://picsum.photos/200',
+      checkinTime: new Date('11-Nov-2021'),
+      checkoutTime: new Date('12-Nov-2021'),
+      rating:4.5
+    },{
+      roomNumber:102,
+      roomType:'Deluxe Room',
+      amenities:'Air Conditioner, Free Wi-Fi, TV, Bathroom, Kitchen',
+      price:1000,
+      photos:'https://picsum.photos/202',
+      checkinTime: new Date('11-Nov-2021'),
+      checkoutTime: new Date('12-Nov-2021'),
+      rating:3.4
+    },{
+      roomNumber:103,
+      roomType:'Private Suite Room',
+      amenities:'Air Conditioner, Free Wi-Fi, TV, Bathroom, Kitchen',
+      price:1500,
+      photos:'https://picsum.photos/203',
+      checkinTime: new Date('11-Nov-2021'),
+      checkoutTime: new Date('12-Nov-2021'),
+      rating:4.7
+    },
+  ]
   
   constructor(){  }
   ngOnInit():void{  }
