1bc12bc81aa950524b01ce1891f9e20853185397
tjiptostevens <steveneonites@gmail.com>
2023-03-29 17:41:40 +0700

    adding navbar

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -4,11 +4,13 @@ import { BrowserModule } from '@angular/platform-browser';
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { RoomsComponent } from './rooms/rooms.component';
+import { NavbarComponent } from './navbar/navbar.component';
 
 @NgModule({
   declarations: [
     AppComponent,
-    RoomsComponent
+    RoomsComponent,
+    NavbarComponent
   ],
   imports: [
     BrowserModule,
